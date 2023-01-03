eb62aa3281de5ea9d60f1db5ba86a82f316b4990
Yuliia <example.com>
2023-01-03 19:38:51 +0200

    Practice

---
+++ src/classes/reference-item.ts
@@ -0,0 +1,45 @@
+/* eslint-disable no-underscore-dangle */
+
+import { timeout } from '../decorators';
+
+abstract class ReferenceItem {
+    // title: string;
+    // year: number;
+
+    // constructor(newTitle: string, newYear: number) {
+    //     console.log('Creating a new ReferenceItem...');
+    //     this.title = newTitle;
+    //     this.year = newYear;
+    // }
+    #id: number; // hard private
+    private _publisher: string; // soft private
+
+    get publisher(): string {
+        return this._publisher.toUpperCase();
+    }
+
+    set publisher(newPublisher: string) {
+        this._publisher = newPublisher;
+    }
+
+    static department: string = 'Research';
+
+    constructor(id: number, public title: string, protected year: number) {
+        console.log('Creating a new ReferenceItem...');
+        this.#id = id;
+    }
+
+    @timeout(2000)
+    printItem(): void {
+        console.log(`${this.title} was published in ${this.year}`);
+        console.log(ReferenceItem.department);
+        console.log(Object.getPrototypeOf(this).constructor.department);
+    }
+    getID(): number {
+        return this.#id;
+    }
+
+    abstract printCitation(): void;
+}
+
+export { ReferenceItem };
